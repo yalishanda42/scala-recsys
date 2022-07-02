194f2c68872d23ac998cbd05809793c5836e1521
Alexander Ignatov <yalishanda@abv.bg>
2022-07-02 16:09:02 +0300

    A cat app

--- build.sbt
+++ build.sbt
@@ -5,4 +5,6 @@ scalaVersion := "3.1.1"
 
 libraryDependencies ++= Seq(
   "org.scalatest" %% "scalatest" % "3.2.9" % Test,
+  "org.typelevel" %% "cats-core" % "2.7.0",
+  "org.typelevel" %% "cats-effect" % "3.3.12",
 )
---
+++ src/main/scala/App.scala
@@ -0,0 +1,13 @@
+import cats.effect.{ExitCode, IO, IOApp}
+
+object RecommenderApp extends IOApp {
+    def run(args: List[String]): IO[ExitCode] =
+        args.match {
+            case List("train") =>
+                IO(println("Training mode.")).as(ExitCode.Success)
+            case List("predict") =>
+                IO(println("Predicting mode.")).as(ExitCode.Success)
+            case _ =>
+                IO(System.err.println("Usage: RecommenderApp train|predict")).as(ExitCode.Error)
+        }
+}
