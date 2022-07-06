c337e27f89d03affa6aa20e4456a1d67167adb70
Alexander Ignatov <yalishanda@abv.bg>
2022-07-06 15:50:12 +0300

    Extra curry, please

--- src/main/scala/utils/Logger.scala
+++ src/main/scala/utils/Logger.scala
@@ -6,11 +6,11 @@ case class Logger(prefix: String = "") {
   enum Level:
     case Info, Error
 
-  def log(level: Level, message: String): IO[Unit] =
+  def log(level: Level)(message: String): IO[Unit] =
     level match
       case Level.Info => IO(println(s"$prefix [INFO] $message"))
       case Level.Error => IO(System.err.println(s"$prefix [ERROR] $message"))
 
-  def logInfo(msg: String) = log(Level.Info, msg)
-  def logError(msg: String) = log(Level.Error, msg)
+  def logInfo = log(Level.Info)(_)
+  def logError = log(Level.Error)(_)
 }
